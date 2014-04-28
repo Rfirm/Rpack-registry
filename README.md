5667e8ac156e76b291f8d0d66c52eef7961c3f4a
howardchi <howardchi@fougood.com>
2014-04-28 13:30:54 +0800

    init start

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Create a project using django
+
+```
+python /usr/local/lib/python2.7/site-packages/django/bin/django-admin.py startproject rpack_register
+```
+
+## Start server
+
+```
+python manage.py runserver
+```
---
+++ rpack_register/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'rpack_register.views.home', name='home'),
+    # url(r'^rpack_register/', include('rpack_register.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('registry.urls')),
+)
