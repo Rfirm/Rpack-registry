84e6d607a6f66becca756e68250be6359a589833
Frank Yang <yangpoan@gmail.com>
2014-06-15 22:31:49 +0800

    modify class name and change username to account

--- registry/models.py
+++ registry/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Registry(models.Model):
+    _id = models.DateTimeField(auto_now_add=True)
+    account = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+
+    class Meta:
+        ordering = ('_id',)
