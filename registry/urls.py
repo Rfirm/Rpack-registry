4a1d5b2a97479a6ce877abe2b7ff4101918c26b4
Frank Yang <yangpoan@gmail.com>
2014-06-15 22:33:29 +0800

    add /signup and /users/{userId}

--- registry/urls.py
+++ registry/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 from registry import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'))
+	url(r'^signup/$', views.SignUp.as_view()),
+    url(r'^users/(?P<userId>[0-9]+/$)', views.RegistryDetail.as_view()),
+)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
