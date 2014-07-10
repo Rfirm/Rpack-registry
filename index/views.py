dca895e45bb76ab6465314f6075a0222dd896d4e
JackHou <vjack070707@gmail.com>
2014-07-10 16:51:56 +0800

    rm index

--- index/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-def index(request):
-    return render_to_response('index/index.html')
