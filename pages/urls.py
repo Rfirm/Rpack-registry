2cad47d0c7d1c1e3d188668581281ef31d7c61ab
JackHou <vjack070707@gmail.com>
2014-07-10 17:00:11 +0800

    fix bug

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from index import views
+from pages import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'index.views.index'),
+    url(r'^$', 'pages.views.index'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
