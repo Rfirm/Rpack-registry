905c003bcb953a2d5fbc234da80db8086b1fa5e5
Frank Yang <yangpoan@gmail.com>
2014-07-11 09:31:22 +0800

    remove _id and account. add username

--- registry/models.py
+++ registry/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Registry(models.Model):
-    _id = models.DateTimeField(auto_now_add=True)
-    account = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=254)
+class Account(models.Model):
+    username = models.CharField(max_length = 20, primary_key = True)
+    password = models.CharField(max_length = 20)
+    email = models.EmailField(max_length = 254)
 
-    class Meta:
-        ordering = ('_id',)
+    def __unicode__(self):
+        return self.username
