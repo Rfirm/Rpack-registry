f4d8b6576f657b7907e543043aead8c157130e06
Frank Yang <yangpoan@gmail.com>
2014-07-11 11:00:26 +0800

    add /users/{user_name}

--- registry/urls.py
+++ registry/urls.py
@@ -4,5 +4,6 @@ from registry import views
 
 urlpatterns = patterns('',
 	url(r'^users/$', views.users, name = 'users'),
+    url(r'^users/(?P<user_name>\S+)/$', views.detail, name = 'detail'),
 )
 
