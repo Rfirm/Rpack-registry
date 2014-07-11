83d4cbc0ebc06b1b5ba2cdf7f9dd9509d55fa6e3
Frank Yang <yangpoan@gmail.com>
2014-07-11 20:27:28 +0800

    change the length of _id from 26 to 36 change the length of username and password from 20 to 100

--- registry/models.py
+++ registry/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    _id = models.CharField(max_length = 26, primary_key = True)
-    username = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
+    _id = models.CharField(max_length = 36, primary_key = True)
+    username = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
     email = models.EmailField(max_length = 254)
 
     def __unicode__(self):
