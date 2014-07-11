3b066302764e3c09c94b8f9bb9fee1a9a2b5b78e
JackHou <vjack070707@gmail.com>
2014-07-11 21:22:48 +0800

    rearrange folder

---
+++ static/javascripts/main/index_out/index_out.js
@@ -0,0 +1,12 @@
+require(["./config"], function(config) {
+
+  require([
+    'jquery',
+    'backbone',
+    'marionette',
+    'underscore',
+    ], function ($, Backbone, Marionette, _){
+
+    });
+})
+
