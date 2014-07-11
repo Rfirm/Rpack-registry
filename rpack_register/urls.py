e95f062d492280cfec78ead21ea46d2c058ee14e
JackHou <vjack070707@gmail.com>
2014-07-11 20:23:49 +0800

    edit the form action

--- rpack_register/urls.py
+++ rpack_register/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^', include('pages.urls')),
-    # url(r'^', include('registry.urls')),
+    url(r'^', include('registry.urls')),
 )
