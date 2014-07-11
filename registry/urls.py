0817cc83c20b6943f0b5944443f89bd22593180e
Frank Yang <yangpoan@gmail.com>
2014-07-11 09:35:15 +0800

    remove /users/{userId}. change /signup to /users

--- registry/urls.py
+++ registry/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, url
-from rest_framework.urlpatterns import format_suffix_patterns
 from registry import views
 
 
 urlpatterns = patterns('',
-	url(r'^signup/$', views.SignUp.as_view()),
-    url(r'^users/(?P<userId>[0-9]+/$)', views.RegistryDetail.as_view()),
+	url(r'^users/$', views.users, name = 'users'),
 )
 
-urlpatterns = format_suffix_patterns(urlpatterns)
