7b2876bc3bcb1bc5ca8f002e29be3be995d88780
Frank Yang <yangpoan@gmail.com>
2014-07-11 11:37:08 +0800

    add _id which is primary key

--- registry/models.py
+++ registry/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    username = models.CharField(max_length = 20, primary_key = True)
+    _id = models.CharField(max_length = 26, primary_key = True)
+    username = models.CharField(max_length = 20)
     password = models.CharField(max_length = 20)
     email = models.EmailField(max_length = 254)
 
