38b3fc991abc5330d45a0feac98f55be66c9bec9
Frank Yang <yangpoan@gmail.com>
2014-07-11 11:38:06 +0800

    add uuid. remove error status.

--- registry/views.py
+++ registry/views.py
@@ -4,19 +4,28 @@ from django.views.decorators.csrf import csrf_exempt
 from registry.models import Account
 from django.shortcuts import get_object_or_404
 import json
+import uuid
 
 @csrf_exempt
 def users(request):
-    accountFilter = Account.objects.filter(pk = request.POST['username'])
+    #Create new account
+    accountFilter = Account.objects.filter(username = request.POST['username'])
 
     if accountFilter.count() > 0 :
-        return HttpResponse("There is same username in database",status = 404)
+        response_data = {}
+        response_data['status'] = "error"
+        response_data['message'] = "The username have been registered."
+        return HttpResponse(json.dumps(response_data), status = 200)
     else:
         try:
-            accountTmp = Account(username = request.POST['username'], password = request.POST['password'], email = request.POST['email'])
+            uuidTmp = uuid.uuid1()
+            accountTmp = Account(_id = str(uuidTmp), username = request.POST['username'], password = request.POST['password'], email = request.POST['email'])
             accountTmp.save()
         except:
-            return HttpResponse("Something error!", status = 404)
+            response_data = {}
+            response_data['status'] = "error"
+            response_data['message'] = "Something error!"
+            return HttpResponse(json.dumps(response_data), status = 400)
         else:
             return HttpResponse(status = 200)
 
