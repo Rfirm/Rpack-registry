2639341de01547477e36388e6917d6a6d7191ccd
JackHou <vjack070707@gmail.com>
2014-07-13 20:27:30 +0800

    fix

---
+++ static/javascripts/main/index/user.js
@@ -0,0 +1,17 @@
+require(["./config"], function(config) {
+
+  require([
+    'jquery',
+    'backbone',
+    'marionette',
+    'underscore',
+    ], function ($, Backbone, Marionette, _){
+        var User = new Marionette.Application();
+
+        User.on("initialize:after", function(){
+            console.log("hi");
+        });
+        User.start();
+    });
+})
+
