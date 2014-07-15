321cf6d0299d44f0256680493f1b05fb99a0a837
JackHou <vjack070707@gmail.com>
2014-07-15 17:30:24 +0800

    edit

--- static/javascripts/main/index/user.js
+++ static/javascripts/main/index/user.js
@@ -7,6 +7,9 @@ require(["./config"], function(config) {
     'underscore',
     ], function ($, Backbone, Marionette, _){
         var User = new Marionette.Application();
+        User.addRegions({
+            mainRegion: '#user_area'
+        })
         // User.on("initialize:after", function(){
         //     console.log("hi");
         // });
